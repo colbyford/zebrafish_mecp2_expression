00ac1ef6e49acf1805ad7277b6abdaf417174781
Colby Ford <colbytylerford@gmail.com>
2022-11-28 15:43:08 -0500

    Update gsea.R

--- gsea/gsea.R
+++ gsea/gsea.R
@@ -1,3 +1,30 @@
-devtools::install_github("GSEA-MSigDB/GSEA_R")
+## Prepare GCT File of Expression Values
+# BiocManager::install("ArrayTools")
+library(ArrayTools)
+library(dplyr)
+library(tibble)
+library(readr)
 
-library
+## Read in Expression Data
+
+data <- read_csv("../rsem_GENE.csv") %>%
+  select(c("Symbol", "gene_id", starts_with("s4d-mecp"))) %>% 
+  rename(NAME = Symbol, Description = gene_id) # %>% 
+  # column_to_rownames("NAME")
+  
+write_tsv(data, "rsem_GENE_shaped.gct")
+
+## Read in Group Info
+
+# groups <- read_tsv("groups.txt") %>% column_to_rownames("X1")
+# 
+# ## Create Expression Set
+# 
+# eSet <- createExpressionSet(pData = groups,
+#                             exprs = data,
+#                             annotation = "hugene10sttranscriptcluster")
+# 
+#   
+# ## output GCT File
+# 
+# output.gct(data, filename = "expresion.gct")
