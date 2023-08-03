781e590887af6708c5e69df69e3b73b9a9a1f48e
Colby Ford <colbytylerford@gmail.com>
2023-08-02 21:49:34 -0400

    Update pathways.R

--- pathways.R
+++ pathways.R
@@ -4,16 +4,16 @@ library(readr)
 
 
 ## DEMO: http://bioconductor.org/packages/release/bioc/vignettes/pathview/inst/doc/pathview.pdf
-data(gse16873.d)
-data(demo.paths)
-
-head(demo.paths,3)
-
-
-i <- 1
-pv.out <- pathview(gene.data = gse16873.d, pathway.id = demo.paths$sel.paths[i],
-                   species = "hsa", out.suffix = "gse16873", kegg.native = T)
-list.files(pattern="hsa04110", full.names=T)
+# data(gse16873.d)
+# data(demo.paths)
+# 
+# head(demo.paths,3)
+# 
+# 
+# i <- 1
+# pv.out <- pathview(gene.data = gse16873.d, pathway.id = demo.paths$sel.paths[i],
+#                    species = "hsa", out.suffix = "gse16873", kegg.native = T)
+# list.files(pattern="hsa04110", full.names=T)
 
 
 
