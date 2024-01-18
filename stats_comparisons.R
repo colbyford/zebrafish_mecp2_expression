5a9dde7f64dd45da74d25da38240b997fb10dda4
Colby Ford <colbytylerford@gmail.com>
2024-01-17 21:51:24 -0500

    Update stats_comparisons.R

--- stats_comparisons.R
+++ stats_comparisons.R
@@ -2,17 +2,65 @@ library(dplyr)
 library(readxl)
 library(stringr)
 
-data <- read_xlsx("021219_mecp2_DF Obend responsiveness-kinematics_d5.xlsx",
-                  sheet = "Obend kin stim 6 to 0") %>% 
+## Lightflash Data
+lf_data <- read_xlsx("mecp2_lightflash_kinematics_data.xlsx",
+                  sheet = "summary-all turns") %>% 
   mutate(group = case_when(
       str_detect(File, "_Mu_") ~ "Mutant",
       TRUE ~ "Wild Type"))
 
-data_group_a <- (data %>% filter(group == "Mutant"))$Lat
-data_group_b <- (data %>% filter(group == "Wild Type"))$Lat
+### Latency
+wilcox.test(
+  (lf_data %>% filter(group == "Mutant"))$Lat,
+  (lf_data %>% filter(group == "Wild Type"))$Lat,
+  correct = TRUE)
 
-# Perform Mann-Whitney U test with correction for ties
-result <- wilcox.test(data_group_a, data_group_b, correct = TRUE)
+### Angle
+wilcox.test(
+  (lf_data %>% filter(group == "Mutant"))$Ang,
+  (lf_data %>% filter(group == "Wild Type"))$Ang,
+  correct = TRUE)
 
-# Display the test result
-print(result)
+### Duration
+wilcox.test(
+  (lf_data %>% filter(group == "Mutant"))$Dur,
+  (lf_data %>% filter(group == "Wild Type"))$Dur,
+  correct = TRUE)
+
+### Max Angular Velocity
+wilcox.test(
+  (lf_data %>% filter(group == "Mutant"))$Mav,
+  (lf_data %>% filter(group == "Wild Type"))$Mav,
+  correct = TRUE)
+
+
+## Darkflash
+df_data <- read_xlsx("mecp2_darkflash_kinematics_data.xlsx",
+                     sheet = "Summary - O-bends") %>% 
+  mutate(group = case_when(
+    str_detect(File, "_Mu_") ~ "Mutant",
+    TRUE ~ "Wild Type"))
+
+### Latency
+wilcox.test(
+  (df_data %>% filter(group == "Mutant"))$Lat,
+  (df_data %>% filter(group == "Wild Type"))$Lat,
+  correct = TRUE)
+
+### Angle
+wilcox.test(
+  (df_data %>% filter(group == "Mutant"))$Ang,
+  (df_data %>% filter(group == "Wild Type"))$Ang,
+  correct = TRUE)
+
+### Duration
+wilcox.test(
+  (df_data %>% filter(group == "Mutant"))$Dur,
+  (df_data %>% filter(group == "Wild Type"))$Dur,
+  correct = TRUE)
+
+### Max Angular Velocity
+wilcox.test(
+  (df_data %>% filter(group == "Mutant"))$Mav,
+  (df_data %>% filter(group == "Wild Type"))$Mav,
+  correct = TRUE)
